fb2d2e2511dd6b46ad1ada0fc9575fbd36a4e76f
triyanarief <triyanarief@icloud.com>
2017-04-10 16:53:17 +0700

    adding component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import ReactDom from 'react-dom';
+import App from './App.jsx';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDom.render(
+  <App/>, document.getElementById('root')
 );
