3f3a9d421a7515b0ef071bf0692edafda385405b
triyanarief <triyanarief@icloud.com>
2017-04-11 15:11:53 +0700

    hardcode

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        <div>Countdown to April 28, 2017</div>
+        <div className="App">
+        <div className="App-title">Countdown to April 28, 2017</div>
         <div>
-          <div>14 Days</div>
-          <div>30 Hours</div>
-          <div>15 Minutes</div>
-          <div>20 Seconds</div>
+          <div className="Clock-days">14 Days</div>
+          <div className="Clock-hours">30 Hours</div>
+          <div className="Clock-minutes">15 Minutes</div>
+          <div className="Clock-seconds">20 Seconds</div>
         </div>
         <div>
           <input placeholder="new date"/>
