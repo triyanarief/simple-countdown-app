ea0f2bedb0d0699142fe09f498c3659d469a2141
triyanarief <triyanarief@icloud.com>
2017-04-11 14:58:24 +0700

    hardcode

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>Countdown Simple App</div>
+      <div>
+        <div>Countdown to April 28, 2017</div>
+        <div>
+          <div>14 Days</div>
+          <div>30 Hours</div>
+          <div>15 Minutes</div>
+          <div>20 Seconds</div>
+        </div>
+        <div>
+          <input placeholder="new date"/>
+          <button>Submit</button>
+        </div>
+      </div>
     )
   }
 };
