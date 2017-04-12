ece608aa45ffb0d623849d88d6decf3ed3553766
triyanarief <triyanarief@icloud.com>
2017-04-12 13:14:48 +0700

    countdown done

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -12,25 +12,38 @@ class Clock extends Component {
       }
   }
 
+  componentWillMount() {
+    this.getTimeUntill(this.props.deadline);
+  }
+
+  componentDidMount() {
+    setInterval(() => this.getTimeUntill(this.props.deadline), 1000)
+  }
+
+  leading0(num) {
+    if (num < 10) {
+      return '0' + num;
+    }
+    return num;
+  }
+
   getTimeUntill(deadline) {
     const time = Date.parse(deadline) - Date.parse(new Date());
-    console.log('time', time);
     const seconds = Math.floor((time/1000)%60);
     const minutes = Math.floor((time/1000/60)%60);
     const hours = Math.floor(time/(1000*60*60)%24);
     const days = Math.floor(time/(1000*60*60*24));
 
-    console.log('seconds', seconds, 'minutes', minutes, 'hours', hours, 'days', days);
+    this.setState({days, hours, minutes, seconds})
   }
 
   render() {
-    this.getTimeUntill(this.props.deadline);
     return (
       <div>
-        <div className="Clock-days">{this.state.days} Days</div>
-        <div className="Clock-hours">{this.state.hours} Hours</div>
-        <div className="Clock-minutes">{this.state.minutes} Minutes</div>
-        <div className="Clock-seconds">{this.state.seconds} Seconds</div>
+        <div className="Clock-days">{this.leading0(this.state.days)} Days</div>
+        <div className="Clock-hours">{this.leading0(this.state.hours)} Hours</div>
+        <div className="Clock-minutes">{this.leading0(this.state.minutes)} Minutes</div>
+        <div className="Clock-seconds">{this.leading0(this.state.seconds)} Seconds</div>
       </div>
     )
   }
